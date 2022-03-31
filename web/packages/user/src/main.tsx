58caad5cc1d875091731ab66a54016fed8b84867
sushao <suxiaoshao@gmial.com>
2022-03-31 09:22:11 +0800

    perf(packages): 更新包

--- web/packages/user/src/main.tsx
+++ web/packages/user/src/main.tsx
@@ -1,7 +1,6 @@
 import { ApolloProvider } from '@apollo/client';
 import { SnackbarProvider, CustomTheme } from 'common';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
@@ -10,21 +9,18 @@ import { client } from './utils/graphql';
 import DateAdapter from '@mui/lab/AdapterDayjs';
 import { LocalizationProvider } from '@mui/lab';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <CustomTheme>
-        <SnackbarProvider>
-          <ApolloProvider client={client}>
-            <BrowserRouter>
-              <LocalizationProvider dateAdapter={DateAdapter}>
-                <App />
-              </LocalizationProvider>
-            </BrowserRouter>
-          </ApolloProvider>
-        </SnackbarProvider>
-      </CustomTheme>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root'),
+createRoot(document.getElementById('root') ?? document.body).render(
+  <Provider store={store}>
+    <CustomTheme>
+      <SnackbarProvider>
+        <ApolloProvider client={client}>
+          <BrowserRouter>
+            <LocalizationProvider dateAdapter={DateAdapter}>
+              <App />
+            </LocalizationProvider>
+          </BrowserRouter>
+        </ApolloProvider>
+      </SnackbarProvider>
+    </CustomTheme>
+  </Provider>,
 );
